dae4b5608f4ecb5d25051d3ab833a081237b2876
Vasily <ilchenkovas@yandex.ru>
2020-03-10 16:35:18 +0700

    add view

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,8 +1,47 @@
 from django.shortcuts import render
 
+from .models import Book, Author, BookInstance, Genre
 # Create your views here.
 
 #from django.http import HttpResponse
 
 #def hello(request)
 #    return HttpResponse("Hello world")
+from django.views import generic
+class BookListView(generic.ListView):
+    model = Book
+
+     def get_queryset(self):
+        return Book.objects.filter(title__icontains='war')[:5] # Получить 5 книг, содержащих 'war' в заголовке
+
+    def get_context_data(self, **kwargs):
+        # В первую очередь получаем базовую реализацию контекста
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Добавляем новую переменную к контексту и иниуиализируем ее некоторым значением
+        context['some_data'] = 'This is just some data'
+        return context
+
+def index(request):
+    """
+    Функция отображения для домашней странницы
+    """
+    # Генерация "количеств" некоторых главных объектов
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    #Доступнык книги (статус = 'a')
+    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
+    num_authors=Author.objects.count()  # Метод 'all()' применен по умолчанию.
+
+    # Отрисовка HTML-шаблона index.html с данными внутри 
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={
+            'num_books':num_books,
+            'num_instances':num_instances,
+            'num_instances_available':num_instances_available,
+            'num_authors':num_authors
+            },
+    )
+
